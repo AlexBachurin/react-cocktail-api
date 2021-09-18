571c36b08a1bc789c1120e95d80d20a46d7301a9
Alex <alexbachurin91@gmail.com>
2021-09-18 10:11:24 +0300

    svg loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
     return (
         <div>
-            <h1>Loading...</h1>
+            <img className="loading" src="https://res.cloudinary.com/dljezd6qv/image/upload/v1631948819/react-cocktails/Bean_Eater-1s-200px_tekkph.svg" alt="loading" />
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SearchForm from '../components/SearchForm'
 import CocktailList from '../components/CocktailList'
 
+
 const Home = () => {
 
     return (
