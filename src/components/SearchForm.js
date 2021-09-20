bb8192f39218580b899796a27b4662e4c9953136
Alex <alexbachurin91@gmail.com>
2021-09-20 13:15:51 +0300

    prevent page reload if user types enter while in search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,9 @@ import { useGlobalContext } from '../context'
 const SearchForm = () => {
     const { setSearchTerm, term } = useGlobalContext();
     return (
-        <form className="search-form">
+        <form onSubmit={(e) => {
+            e.preventDefault();
+        }} className="search-form">
             <div className="form-control">
                 <label htmlFor="name">search cocktail</label>
                 <input value={term} onChange={setSearchTerm} type="text" id="name" name="name" />
