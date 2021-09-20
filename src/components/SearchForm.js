1514311c317f201d9ea308aa929cd52aeb9bd3ca
Alex <alexbachurin91@gmail.com>
2021-09-20 13:19:11 +0300

    input focus on page load

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,13 +3,19 @@ import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
     const { setSearchTerm, term } = useGlobalContext();
+    const inputRef = React.useRef(null);
+
+    //set focus on input on page load
+    React.useEffect(() => {
+        inputRef.current.focus();
+    }, [])
     return (
         <form onSubmit={(e) => {
             e.preventDefault();
         }} className="search-form">
             <div className="form-control">
                 <label htmlFor="name">search cocktail</label>
-                <input value={term} onChange={setSearchTerm} type="text" id="name" name="name" />
+                <input ref={inputRef} value={term} onChange={setSearchTerm} type="text" id="name" name="name" />
             </div>
         </form>
     )
